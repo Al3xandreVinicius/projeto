77415068b52bfad661805d68cbe8fb10a8337c30
Al3xandreVinicius <al3xandrevinicius@gmail.com>
2024-05-05 10:43:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")  //mudar de dark para light
+   // if(html.classList.contains('light')){
+   //     html.classList.remove('light')
+   // }else{
+   //     html.classList.add('light')
+   // }
+
+
+   // Mudar Imagem de Fundo **
+   // Se tiver light mode, adicionar a imagem light
+   // se tiver sem light mode, mander a imagem normal
+
+   //pegar a tag img
+   const img = document.querySelector("#profile img") // o argumento que vai dentro é o mesmo seletor do CSS
+   const textFoto = document.querySelector("#profile img") // substituir texto de descrição da imagem
+   
+   //substituir a imagem
+   if(html.classList.contains("light")){
+    // se tiver light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    textFoto.setAttribute("alt", "Foto com fundo escuro, Usando oculos escuro")
+
+   }else{
+    //se tiver sem light mode
+    img.setAttribute("src", "./assets/avatar.png")
+    textFoto.setAttribute("alt", "Foto com fundo amarelo, Usando oculos normal ")
+
+   }
+
+
+
+}
